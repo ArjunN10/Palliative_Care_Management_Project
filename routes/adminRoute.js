46549faf259d88ccd0e4e4c30189598dba0148df
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2024-02-28 13:45:02 +0530

    fea : error handler middleware

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,11 +1,24 @@
-const router = require("express").Router();
+const router = require("express").Router()
 
 const adminController=require("../controllers/adminController")
 const medicineController=require("../controllers/medicineController")
-const {isAdmin,loggedOutAdmin}=require("../middlewares/auth")
+const TrycatchMiddleware=require("../middlewares/TryCatch")
+
+const {isAdmin,loggedInAdmin}=require("../middlewares/auth")
 
 
-// router.get("/login", loggedOutAdmin, adminController.loadLogin);
+
+router
+
+.use(TrycatchMiddleware)
+
+.get("/login", loggedInAdmin, adminController.loadLogin)
+.post("/login", loggedInAdmin, adminController.AdminLogin)
+
+//.post("/dashboard", isAdmin, adminController.    );
+
+
+
 
 
 module.exports=router
--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -1,40 +1,45 @@
 const router = require("express").Router();
 const doctorController = require("../controllers/doctorController");
 const medicineController = require("../controllers/medicineController");
+const TrycatchMiddleware=require("../middlewares/TryCatch")
+
 const { isDoctor, loggedOutDoctor } = require("../middlewares/auth");
 
-router.get("/login", loggedOutDoctor, doctorController.loadLogin);
-router.post("/login", loggedOutDoctor, doctorController.DoctorLogin);
+router
+.use(TrycatchMiddleware)
+.get("/login", loggedOutDoctor, doctorController.loadLogin)
 
-router.get("/dashboard", isDoctor, doctorController.dashboard);
+.post("/login", loggedOutDoctor, doctorController.DoctorLogin)
 
-router.get("/users/:id/edit", isDoctor, doctorController.loadEditUser);
-router.put("/users/:id", isDoctor, doctorController.updateUser);
-router.delete("/users/:id/destroy", isDoctor, doctorController.deleteUser);
+.get("/dashboard", isDoctor, doctorController.dashboard)
+.put("/users/:id", isDoctor, doctorController.updateUser)
+.get("/users/:id/edit", isDoctor, doctorController.loadEditUser)
+.delete("/users/:id/destroy", isDoctor, doctorController.deleteUser)
 
-router.get("/createUser", isDoctor, doctorController.DoctorAddUser);
-router.post("/createUser", isDoctor, doctorController.createUser);
+.post("/createUser", isDoctor, doctorController.createUser)
+.get("/createUser", isDoctor, doctorController.DoctorAddUser)
 
 //patients
-router.get("/addPatient", isDoctor, doctorController.getAddPatient);
-router.post("/addPatient", isDoctor, doctorController.AddPatient);
-router.get("/patients", isDoctor, doctorController.getPatientsList);
-router.post("/searchPatients", isDoctor, doctorController.searchPatient);
-router.get("/patients/:id/edit", isDoctor, doctorController.editPatient);
-router.post("/updatePatient", isDoctor, doctorController.updatePatient);
-router.delete("/patients/:id/destroy", isDoctor, doctorController.deletePatient);
+
+.post("/addPatient", isDoctor, doctorController.AddPatient)
+.get("/addPatient", isDoctor, doctorController.getAddPatient)
+.get("/patients", isDoctor, doctorController.getPatientsList)
+.post("/searchPatients", isDoctor, doctorController.searchPatient)
+.get("/patients/:id/edit", isDoctor, doctorController.editPatient)
+.post("/updatePatient", isDoctor, doctorController.updatePatient)
+.delete("/patients/:id/destroy", isDoctor, doctorController.deletePatient)
 
 //medicine
-router.get("/medicines", isDoctor, medicineController.showMedicines);
-router.get("/addMedicine", isDoctor, medicineController.ShowAddMedicine);
-router.post("/createMedicine", isDoctor, medicineController.addMedicine);
-router.get("/medicines/:id/edit", isDoctor, medicineController.showEditMed);
-router.post("/editMedicine", isDoctor, medicineController.updateMedicine);
-router.delete("/medicines/:id/destroy",isDoctor,medicineController.deleteMedicine);
-router.post("/medicines/search", isDoctor, medicineController.searchMedicine);
+.get("/medicines", isDoctor, medicineController.showMedicines)
+.get("/addMedicine", isDoctor, medicineController.ShowAddMedicine)
+.post("/createMedicine", isDoctor, medicineController.addMedicine)
+.get("/medicines/:id/edit", isDoctor, medicineController.showEditMed)
+.post("/editMedicine", isDoctor, medicineController.updateMedicine)
+.delete("/medicines/:id/destroy",isDoctor,medicineController.deleteMedicine)
+.post("/medicines/search", isDoctor, medicineController.searchMedicine)
 
 
-router.get("/medicineHistory", isDoctor, doctorController.medicineHistory);
-router.post("/logout", isDoctor, doctorController.logoutDoctor);
+.get("/medicineHistory", isDoctor, doctorController.medicineHistory)
+.post("/logout", isDoctor, doctorController.logoutDoctor)
 
 module.exports = router;
