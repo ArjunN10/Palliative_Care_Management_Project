61e3e2ea1f9061fe03d4c78f3e9afca1b1b94e1d
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2024-03-08 13:07:50 +0530

    feat : admin volunteer management

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -10,14 +10,28 @@ const {isAdmin,loggedInAdmin}=require("../middlewares/auth")
 
 router
 
+// ===============================< Login/Logout >================================//
 
 .get("/login", loggedInAdmin,TrycatchMiddleware(adminController.loadLogin) )
 .post("/login", loggedInAdmin, TrycatchMiddleware(adminController.AdminLogin))
 
-.get("/dashboard",isAdmin,TrycatchMiddleware(adminController.AdminDashboard));
+.post("/logout", isAdmin, TrycatchMiddleware( adminController.logoutAdmin))
 
 
+// ===============================< Dashboard >================================//
 
+.get("/dashboard",isAdmin,TrycatchMiddleware(adminController.AdminDashboard))
+
+// ===============================< Staff Management >================================//
+
+.post("/createVolunteers",isAdmin,TrycatchMiddleware(adminController.createVolunteer) )
+.get("/createVolunteers", isAdmin,TrycatchMiddleware(adminController.ViewVolunteer) )
+
+.get("/volunteers/:id/edit",isAdmin,TrycatchMiddleware(adminController.loadEditVolunteer) )
+.put("/volunteers/:id",isAdmin,TrycatchMiddleware( adminController.updateVolunteer))
+.delete("/volunteers/:id/destroy",isAdmin,TrycatchMiddleware(adminController.deleteVolunteer))
+
+// ===============================< Doctor Management >================================//
 
 
 module.exports=router
