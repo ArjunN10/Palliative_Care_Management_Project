ded4ffc68af2c35a3d18d64f31f4708b680507c0
hornto <krishnadas10.official@gmail.com>
2024-03-09 12:54:20 +0530

    added edit and delete the patient

--- controllers/LaboratoryStaffController.js
+++ controllers/LaboratoryStaffController.js
@@ -9,19 +9,10 @@ const loadLogin = (req, res) => {
     res.render("staff/login", { error: null, message: null });
   };
 
-  const securePassword = async (password) => {
-    try {
-      const passwordHash = await bcrypt.hash(password, 10);
-      return passwordHash;
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
 const staffLogin = async (req,res)=>{
 
   const {email,password} = req.body
-    try {
+    
         const staff = await User.findOne({email})
         if(!staff)
             return res.render ('staff/login',{
@@ -42,18 +33,11 @@ const staffLogin = async (req,res)=>{
         }else {
             res.render("/login?error=" + encodeURIComponent("You're nor laboratary staff"));
           }
-          
-        
-        
-    } catch (error) {
-        console.log(error)
-    }
-     
 };
 
 const dashboard = async (req, res) => {
   const { q } = req.query;
-  try {
+  
     let users;
     if (q && q.length > 0) {
       users = await User.find({
@@ -64,26 +48,22 @@ const dashboard = async (req, res) => {
       users = await User.find({ is_Lab_Staff: 1 });
     }
     res.render('staff/dashboard', { users, q });
-  } catch (error) {
-    console.log(error.message);
-  }
+  
 };
 
 const getAddPatient = async (req, res) => {
-  try {
+  
     const today = new Date();
-
     const medicines = await Medicine.find({
       expiry: { $gt: today }
     });
-        res.render("staff/addPatient", { error: null, message: null, medicines });
-  } catch (error) {
-    console.log(error.message);
-  }
+      
+    res.render("staff/addPatient", { error: null, message: null, medicines });
+  
 };
 
 const getPatientsList = async (req, res) => {
-  try {
+  
     const patients = await Patient.aggregate([
       {
         $lookup: {
@@ -100,14 +80,12 @@ const getPatientsList = async (req, res) => {
       error: null,
       message: null,
     });
-  } catch (error) {
-    console.log(error.message);
-  }
+  
 };
 
 const searchPatient = async (req, res) => {
   const { q } = req.body;
-  try {
+  
     let patients;
 
     if (q) {
@@ -140,15 +118,12 @@ const searchPatient = async (req, res) => {
     }
 
     res.render("staff/dashboard", { patients, message: null, error: null });
-  } catch (error) {
-    console.log(error.message);
-  }
+  
 };
 
 const AddPatient = async (req,res) => {
   let {name, mobile, disease, DoctorName, selectedMedicines} = req.body
-  console.log(req.body)
-  try {
+  
     function generateRandomID(length) {
       const charset = "0123456789";
       let randomID = "";
@@ -174,8 +149,8 @@ const AddPatient = async (req,res) => {
       disease,
       mobile,
       DoctorName,
-      Medicine : selectedMedicines ? selectedMedicines.map(() => ({
-
+      Medicine : selectedMedicines ? selectedMedicines.map((medId) => ({
+       medicine : medId
       })):null ,
       addingDate : Date.now()
       
@@ -184,11 +159,85 @@ const AddPatient = async (req,res) => {
     await patient.save();
     return res.redirect("/staff/dashboard");
 
-    
-  } catch (error) {
-    console.log(error)
+}; 
+
+const editPatient  = async (req, res) => {
+  const { id } = req.params;
+  
+    const Pid = await Patient.findById(id);
+    const patient = await Patient.aggregate([
+      {
+        $match: {
+          _id: Pid._id,
+        },
+      },
+      {
+        $lookup: {
+          from: "medicines",
+          localField: "Medicines.medicine",
+          foreignField: "_id",
+          as: "Medicines.medicine", // Store the medicine details in an array
+        },
+      },
+    ]);
+
+    const currentMedicines = patient[0].Medicines;
+
+    if (patient.length === 0) {
+      return res.status(404).send("Patient not found");
+    }
+
+    const medicines = await Medicine.find();
+    res.render("staff/editPatient", {
+      currentMedicines,
+      patient,
+      medicines,
+      error: null,
+      message: null,
+    });
+ 
+};
+
+const updatePatient = async (req,res) => {
+  
+  let { id,name, disease, mobile, DoctorName, selectedMedicines } = req.body;
+     if(name.length < 3) {
+      return res.render ("staff/addPatient", {
+        error : "name should contain atleast 3 letters" ,
+      })
+    }
+
+     if(typeof selectedMedicines === "string"){
+      let medicine = selectedMedicines
+      selectedMedicines = [] 
+      selectedMedicines.push(medicine)
+    }
+
+    await Patient.findByIdAndUpdate (
+      id ,
+      {
+        $set : {
+          name, 
+          disease,
+          mobile,
+          DoctorName,
+          Medicines: selectedMedicines?selectedMedicines.map((medId) => ({
+            medicine: medId,
+          })):null,
+        },
+      },
+      {new : true}
+     );
+     res.redirect("/staff/dashboard")
+  };
+
+const deletePatient = async (req,res) => {
+  const {id} = req.params 
+    await Patient.findByIdAndDelete(id)
+    res.redirect("/staff/dashboard")
   }
-}
+
+
 
 
 module.exports = {
@@ -198,7 +247,10 @@ module.exports = {
   getAddPatient,
   AddPatient,
   getPatientsList,
-  searchPatient
+  searchPatient,
+  editPatient,
+  updatePatient,
+  deletePatient
 }
 
 
--- routes/staffRoute.js
+++ routes/staffRoute.js
@@ -1,20 +1,21 @@
 const router = require('express').Router()
 const LaboratoryStaff = require('../controllers/LaboratoryStaffController')
 const { isLaboratoryStaff, loggedOutLaboratoryStaff } = require("../middlewares/auth");
+const trycatch = require("../middlewares/TryCatch")
 
 
-router.get("/login", loggedOutLaboratoryStaff, LaboratoryStaff.loadLogin)
-router.post("/login",loggedOutLaboratoryStaff,LaboratoryStaff.staffLogin)
+router.get("/login", loggedOutLaboratoryStaff,trycatch(LaboratoryStaff.loadLogin))
+router.post("/login",loggedOutLaboratoryStaff,trycatch(LaboratoryStaff.staffLogin))
 
-// router.get("/dashboard",isLaboratoryStaff,LaboratoryStaff.dashboard)
+// patients 
 
-
-router.get("/addPatient", isLaboratoryStaff,LaboratoryStaff.getAddPatient);
-router.post("/addPatient",isLaboratoryStaff,LaboratoryStaff.AddPatient)
-router.get("/dashboard", isLaboratoryStaff, LaboratoryStaff.getPatientsList);
-router.post("/searchPatients" ,isLaboratoryStaff ,LaboratoryStaff.searchPatient) ;
-
-
+router.get("/addPatient", isLaboratoryStaff,trycatch(LaboratoryStaff.getAddPatient));
+router.post("/addPatient",isLaboratoryStaff,trycatch(LaboratoryStaff.AddPatient));
+router.get("/dashboard", isLaboratoryStaff,trycatch (LaboratoryStaff.getPatientsList));
+router.post("/searchPatients" ,isLaboratoryStaff,trycatch(LaboratoryStaff.searchPatient));
+router.get ("/dashboard/:id/edit",isLaboratoryStaff,trycatch(LaboratoryStaff.editPatient));
+router.post ("/updatePatient",isLaboratoryStaff,trycatch(LaboratoryStaff.updatePatient));
+router.delete("/dashboard/:id/destroy",isLaboratoryStaff,trycatch(LaboratoryStaff.deletePatient));
 
 
 
